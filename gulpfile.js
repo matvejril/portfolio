fa3b032184d6b5f61eab52b1f04ddce578423d37
MatvejRil <matvej.ril@gmail.com>
2017-10-19 00:42:06 +0700

    finished project

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,36 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+// var concat = require('gulp-concat');
 var browserSync = require('browser-sync').create();
 
+
 var configs = {
+    src: {
+        less: 'less/main.less'
+    },
+    dist: {
+        css: 'css'
+    },
     watch: {
-        css: 'css/*.css',
         html: 'index.html',
-        scripts: 'scripts/*.js'
+        less: 'less/*.less',
+        css: 'css/main.css'
     }
 };
 
-gulp.task('default', function() {
+
+gulp.task('default', ['less'], function() {
     browserSync.init({
         server: "./"
     });
-    gulp.watch([configs.watch.html, configs.watch.css]).on('change', browserSync.reload)
+    gulp.watch(configs.watch.html).on('change', browserSync.reload);
+    gulp.watch(configs.watch.less, ['less']);
 });
+
+gulp.task('less', function() {
+    return gulp.src(configs.src.less)
+        .pipe(less())
+        .pipe(gulp.dest(configs.dist.css))
+        .pipe(browserSync.stream({ match: '**/*.css' }));
+});
+
